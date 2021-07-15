fa8766a3a470108075683a4f3302d516c0d3fd7a
xywswsxy <37827199+xywswsxy@users.noreply.github.com>
2021-07-15 22:45:17 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,48 @@
+import argparse
+from model import *
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+
+class DatasetFolder(Dataset):
+    def __init__(self, dataset, ids):
+        self.dataset = dataset
+        self.ids = ids
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, item):
+        images, labels = self.dataset[self.ids[item]]
+        return images, labels
+
+def Test(args, w, dataset = None, ids = None):
+    dataloader = DataLoader(DatasetFolder(dataset, ids), batch_size = args.num_batch, shuffle=True)
+
+    if args.dataset == 'mnist':
+        Net = Net_mnist
+    if args.dataset == 'cifar':
+        Net = Net_cifar
+
+    net = Net()
+    net.load_state_dict(w)
+    net.eval()
+    
+    loss_test = 0
+    correct = 0
+    
+    for data in dataloader:
+        images, labels = data
+        
+        log_probs = net(images)
+        loss_test += F.cross_entropy(log_probs, labels, reduction='sum').item()
+        y_pred = log_probs.data.max(1, keepdim=True)[1]
+        correct += y_pred.eq(labels.data.view_as(y_pred)).long().cpu().sum()
+        
+    loss_test /= len(dataloader.dataset)
+    acc = 100.00 * correct / len(dataloader.dataset)
+    
+    return acc, loss_test
